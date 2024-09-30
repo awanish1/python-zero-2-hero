c4f9e297db60e4907f5bac52f6ccaeb46769e0c9
Awanish Pandey <awanish.pandey@traydstream.com>
2024-09-30 14:49:38 +0530

    Lab1-Basic syntax

---
+++ Lab1.py
@@ -0,0 +1,28 @@
+# Basic Python 3 program
+
+# Print a greeting
+print("Hello, World!")
+
+# Perform basic arithmetic operations
+a = 10
+b = 5
+
+# Addition
+addition = a + b
+print("Addition:", addition)
+
+# Subtraction
+subtraction = a - b
+print("Subtraction:", subtraction)
+
+# Multiplication
+multiplication = a * b
+print("Multiplication:", multiplication)
+
+# Division
+division = a / b
+print("Division:", division)
+
+# Modulus
+modulus = a % b
+print("Modulus:", modulus)
